41c682dab048b7c7c187873c009e27638009e874
Matthew Miner <matthew@matthewminer.com>
2020-07-01 21:48:30 -0700

    Organize brew.sh file

--- brew.sh
+++ brew.sh
@@ -1,39 +1,39 @@
 #!/usr/bin/env bash
 
+set -e
+
 brew update
 brew upgrade
 
-# CLI Programs
-brew install bat
-brew install fzf
-brew install git
-brew install git-lfs
-brew install htop
-brew install httpie
-brew install lua
-brew install ncdu
-brew install noti
-brew install pinentry-mac # https://stackoverflow.com/a/40066889
-brew install prettyping
-brew install python
-brew install python3
-brew install ripgrep
-brew install ruby
-brew install swiftlint
-brew install switchaudio-osx
-brew install tmux
-brew install tldr
-brew install tree
-brew install vim
-brew install youtube-dl
-brew install zsh
+brew install \
+    bat \
+    fzf \
+    git \
+    git-lfs \
+    htop \
+    httpie \
+    lua \
+    noti \
+    pinentry-mac \
+    prettyping \
+    python3 \
+    ripgrep \
+    ruby \
+    swiftlint \
+    switchaudio-osx \
+    tmux \
+    tldr \
+    tree \
+    vim \
+    youtube-dl \
+    zsh
 
-# QuickLook Plugins
-brew cask install betterzipql
-brew cask install qlcolorcode
-brew cask install qlmarkdown
-brew cask install qlstephen
-brew cask install quicklook-csv
-brew cask install quicklook-json
+brew cask install \
+    betterzipql \
+    qlcolorcode \
+    qlmarkdown \
+    qlstephen \
+    quicklook-csv \
+    quicklook-json
 
 brew cleanup
