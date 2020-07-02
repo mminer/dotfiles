74f0e09cf318debd7078c0fee5078c9e794e55fe
Matthew Miner <matthew@matthewminer.com>
2020-07-02 09:29:53 -0700

    Install NVM via Git submodule

--- node.sh
+++ node.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-# Install NVM and latest Node LTS release.
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+cd ~/.nvm
+git checkout v0.35.3
+source ~/.zshrc
+
 nvm install --lts
 
 npm install --global \
--- setup.sh
+++ setup.sh
@@ -15,6 +15,7 @@ ln -s ${PWD}/.gitconfig ~/.gitconfig
 ln -s ${PWD}/.gitignore ~/.gitignore
 ln -s ${PWD}/.hushlogin ~/.hushlogin
 ln -s ${PWD}/.ideavimrc ~/.ideavimrc
+ln -s ${PWD}/.nvm ~/.nvm
 ln -s ${PWD}/.oh-my-zsh ~/.oh-my-zsh
 ln -s ${PWD}/.ripgreprc ~/.ripgreprc
 ln -s ${PWD}/.ssh/config ~/.ssh/config
