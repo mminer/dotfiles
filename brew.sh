225adf0bf7114291acec9de6890ab9a3d107572a
Matthew Miner <matthew@matthewminer.com>
2021-01-04 16:14:25 -0800

    Update brew.sh to reflect currently-used packages

--- brew.sh
+++ brew.sh
@@ -2,46 +2,56 @@
 
 set -e
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew update
 brew upgrade
 
+# Update versions of built-in programming languages.
+brew install \
+    python3 \
+    ruby
+
+# Update versions of other built-in tools.
+brew install \
+    vim \
+    zsh
+
+# Install Git and supporting tools.
+brew install \
+    diff-so-fancy \
+    gh \
+    git \
+    git-lfs
+
+# Install fzf and its key bindings.
+# https://github.com/junegunn/fzf#using-homebrew-or-linuxbrew
+brew install fzf
+
+$(brew --prefix)/opt/fzf/install --completion --key-bindings --no-update-rc \
+    --no-bash --no-fish
+
+# Required by the Launchbar Switch Audio action.
+# https://renaghan.com/launchbar/switch-audio/
+brew install switchaudio-osx
+
+# Install other oft-used tools.
 brew install \
     bat \
-    fzf \
-    git \
-    git-lfs \
-    github/gh/gh \
+    dos2unix \
+    fasd \
+    ffmpeg \
     htop \
     httpie \
     lua \
     noti \
     pinentry-mac \
     prettyping \
-    python3 \
     ripgrep \
-    ruby \
     swiftlint \
-    switchaudio-osx \
-    tmux \
     tldr \
+    tmux \
     tree \
-    vim \
-    youtube-dl \
-    zsh
-
-# Install key bindings for fzf.
-# https://github.com/junegunn/fzf#using-homebrew-or-linuxbrew
-$(brew --prefix)/opt/fzf/install --completion --key-bindings --no-update-rc \
-    --no-bash --no-fish
-
-brew cask install \
-    betterzipql \
-    qlcolorcode \
-    qlmarkdown \
-    qlstephen \
-    quicklook-csv \
-    quicklook-json
+    youtube-dl
 
 brew cleanup
