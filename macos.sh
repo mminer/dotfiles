3d53e26e3ede6d1fe62ccb3363da79adb88263a1
Matthew Miner <matthew@matthewminer.com>
2021-01-04 16:51:09 -0800

    Set macOS config to drag windows using gesture

--- macos.sh
+++ macos.sh
@@ -2,7 +2,7 @@
 
 chflags nohidden ~/Library
 
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+defaults write -globalDomain AppleShowAllExtensions -bool true
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 defaults write com.apple.dock show-process-indicators -bool false
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
@@ -17,7 +17,7 @@ defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
 # Dim hidden applications in the Dock.
-defaults write com.apple.Dock showhidden -bool YES
+defaults write com.apple.Dock showhidden -bool true
 
 # Use column view in all Finder windows by default.
 defaults write com.apple.finder FXPreferredViewStyle -string "clmv"
@@ -28,7 +28,10 @@ defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
 defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
 
 # Expand save and print panels by default.
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
-defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write -globalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write -globalDomain PMPrintingExpandedStateForPrint -bool true
+
+# Support dragging a window from anywhere by holding command-control-option while dragging.
+defaults write -globalDomain NSWindowShouldDragOnGesture true
 
 killall Dock
